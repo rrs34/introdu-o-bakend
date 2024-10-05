8cc860f88cb84d52e82542aca943bd7702534c86
Renata Rocha da Silva <rr.rocha1987@gmail.com>
2024-10-05 14:44:55 +0000

    Adicionar calaculadora php

---
+++ .devcontainer/calculadora.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+</head>
+<body>
+    <h1>Calculadora FullStack</h1>
+    <form action="calculadora.php" method="post">    
+    <label>Numero 1</label>
+        <input type="text" name="numero1" id="numero1"></input>
+
+        <label>Numero 2</label>
+        <input type="text" name="numero2" id="numero2"></input>
+
+        <input type="submit" value="Calcular">
+    </form>   
+    <hr/>
+    
+
+</body>
+</html>
